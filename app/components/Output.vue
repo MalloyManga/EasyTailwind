5e50c740f904f66c563f99e670d651ab7eb8173f
Malloy <li3210361050@gmail.com>
2025-09-01 23:31:05 +0800

    修改了一部分样式 更加像素风

--- app/components/Output.vue
+++ app/components/Output.vue
@@ -38,6 +38,14 @@ const useCheck2 = (val: string) => {
         return false
     }
 }
+const handleClick = async (className: string) => {
+    try {
+        await navigator.clipboard.writeText(className)
+        // 这里需要一个 toast 
+    } catch (error) {
+        // 这里需要一个 toast 
+    }
+}
 
 const twCss = computed(() => {
     if (rawColor) {
@@ -60,13 +68,14 @@ const twCss = computed(() => {
 <template>
     <div v-if="rawColor" class="ize-full grid grid-cols-4 auto-rows-[minmax(2rem,auto)] gap-4">
         <button v-for="tempTwClassName in tempTwClassNames" :class="tempTwClassName.gridAttr" type="button"
-            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.5)_inset]">
+            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.3)_inset,-4px_-4px_#656ccb_inset] active:bg-[#656ccb] active:shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,-4px_-4px_rgba(255,255,255,0.3)_inset]">
             {{ twCss }}
         </button>
     </div>
     <div v-else class="size-full grid grid-cols-4 auto-rows-[minmax(2rem,auto)] gap-4">
-        <button v-for="tempTwClassName in tempTwClassNames" :class="tempTwClassName.gridAttr" type="button"
-            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.5)_inset]">
+        <button v-for="tempTwClassName in tempTwClassNames" :class="tempTwClassName.gridAttr"
+            @click="handleClick(tempTwClassName.className)" type="button"
+            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.3)_inset,-4px_-4px_#656ccb_inset] active:bg-[#656ccb] active:shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,-4px_-4px_rgba(255,255,255,0.3)_inset]">
             {{ tempTwClassName.className }}
         </button>
 
