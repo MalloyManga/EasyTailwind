da1f651b92aaabbe372f2ee5483e5a3925ac296d
Malloy <li3210361050@gmail.com>
2025-09-01 22:53:10 +0800

    添加了一部分样式以及颜色合理性检测

--- app/components/Frame.vue
+++ app/components/Frame.vue
@@ -13,7 +13,7 @@ const handleCss = (val: string) => {
 
 <template>
     <div
-        class="bg-[#f5f5f5] grid grid-rows-[1fr_5fr] items-start w-sm h-128 p-6 shadow-[0_4px_#cccccc,4px_0_#cccccc,-4px_0_#cccccc,0_-4px_#cccccc]">
+        class="bg-[#f5f5f5] grid grid-rows-[1fr_5fr] gap-8 items-start h-fit p-6 shadow-[0_4px_#cccccc,4px_0_#cccccc,-4px_0_#cccccc,0_-4px_#cccccc]">
         <Input @emit-css="handleCss" />
         <Output :raw-color="optVal" />
     </div>
--- app/components/Output.vue
+++ app/components/Output.vue
@@ -4,15 +4,53 @@ interface Prop {
     rawColor: string
 }
 
+interface TempTwClassName {
+    className: string
+    gridAttr: string
+}
+
 const { rawColor } = defineProps<Prop>()
+
+const tempTwClassNames: TempTwClassName[] = [
+    { className: 'bg-[#f5f5f5]', gridAttr: 'col-start-1 col-end-3' },
+    { className: 'border-[#f5f5f5]', gridAttr: 'col-start-3 col-end-5' },
+    { className: 'shadow-[#f5f5f5]', gridAttr: 'col-start-1 col-end-5' },
+    { className: 'text-[#f5f5f5]', gridAttr: 'col-start-1 col-end-3' },
+    { className: 'ring-[#f5f5f5]', gridAttr: 'col-start-3 col-end-5' },
+]
+
+// 两个函数用于检查颜色值为合理并且可用的的
+const useCheck = (val: string) => {
+    // 包含#的颜色检查
+    if (val.length == 4 || val.length == 7) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+const useCheck2 = (val: string) => {
+    // 不包含#的颜色检查
+    if (val.length == 3 || val.length == 6) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 const twCss = computed(() => {
     if (rawColor) {
+        // 该if仅用于绕开ts检测
         if (rawColor.includes('#')) {
-            return `bg-[${rawColor}]`
+            if (useCheck(rawColor)) {
+                return `bg-[${rawColor}]`
+            }
         }
         else {
-            return `bg-[#${rawColor}]`
+            if (useCheck2(rawColor)) {
+                return `bg-[#${rawColor}]`
+            }
         }
     }
 })
@@ -20,13 +58,18 @@ const twCss = computed(() => {
 </script>
 
 <template>
-    <div v-if="rawColor" class="bg-pink-300">
-        {{ twCss }}
-    </div>
-    <div v-else class="size-full bg-pink-300">
-        <button type="button" class="cursor-pointer ">
-            bg-[#f5f5f5]
+    <div v-if="rawColor" class="ize-full grid grid-cols-4 auto-rows-[minmax(2rem,auto)] gap-4">
+        <button v-for="tempTwClassName in tempTwClassNames" :class="tempTwClassName.gridAttr" type="button"
+            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.5)_inset]">
+            {{ twCss }}
         </button>
+    </div>
+    <div v-else class="size-full grid grid-cols-4 auto-rows-[minmax(2rem,auto)] gap-4">
+        <button v-for="tempTwClassName in tempTwClassNames" :class="tempTwClassName.gridAttr" type="button"
+            class="cursor-pointer h-fit flex justify-center items-center text-white bg-[#757de8] px-4 py-3 shadow-[0_4px_#3F51B5,4px_0_#3F51B5,-4px_0_#3F51B5,0_-4px_#3F51B5,4px_4px_rgba(255,255,255,0.5)_inset]">
+            {{ tempTwClassName.className }}
+        </button>
+
     </div>
 
 </template>
